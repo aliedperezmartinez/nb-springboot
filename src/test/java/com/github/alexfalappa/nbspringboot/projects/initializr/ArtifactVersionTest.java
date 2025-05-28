1f224d041f27f3a9bc131235a203cb5db8d54dff
Alied Perez Martinez <aliedperezmartinez@gmail.com>
2025-05-27 21:03:51 -0400

    #1 Add HashCode test

--- src/test/java/com/github/alexfalappa/nbspringboot/projects/initializr/ArtifactVersionTest.java
+++ src/test/java/com/github/alexfalappa/nbspringboot/projects/initializr/ArtifactVersionTest.java
@@ -34,7 +34,6 @@ public class ArtifactVersionTest {
     public void testEquals1() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 4, 2);
         final ArtifactVersion a2 = new ArtifactVersion(1, 4, 2);
-        System.out.format("%s equals %s\n", a1, a2);
         assertEquals(a1, a2);
     }
 
@@ -42,7 +41,6 @@ public class ArtifactVersionTest {
     public void testEquals2() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 1, 3, "SNAPSHOT");
         final ArtifactVersion a2 = new ArtifactVersion(1, 1, 3, "SNAPSHOT");
-        System.out.format("%s equals %s\n", a1, a2);
         assertEquals(a1, a2);
     }
 
@@ -50,14 +48,12 @@ public class ArtifactVersionTest {
     public void testEquals3() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 1, 3, "SNAPSHOT");
         final ArtifactVersion a2 = new ArtifactVersion(1, 1, 3, "snapshot");
-        System.out.format("%s equals %s\n", a1, a2);
         assertEquals(a1, a2);
     }
 
     @Test
     public void testEqualsSame() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 1, 3, "SNAPSHOT");
-        System.out.format("%s equals %s\n", a1, a1);
         assertEquals(a1, a1);
     }
 
@@ -65,7 +61,6 @@ public class ArtifactVersionTest {
     public void testNotEquals1() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 4, 2);
         final ArtifactVersion a2 = new ArtifactVersion(1, 4, 2, "M1");
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
@@ -73,7 +68,6 @@ public class ArtifactVersionTest {
     public void testNotEquals2() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 4, 2, "M2");
         final ArtifactVersion a2 = new ArtifactVersion(1, 4, 2, "M1");
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
@@ -81,7 +75,6 @@ public class ArtifactVersionTest {
     public void testNotEqualsMajor() {
         final ArtifactVersion a1 = new ArtifactVersion(2, 4, 2);
         final ArtifactVersion a2 = new ArtifactVersion(1, 4, 2);
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
@@ -89,7 +82,6 @@ public class ArtifactVersionTest {
     public void testNotEqualsMinor() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 5, 2);
         final ArtifactVersion a2 = new ArtifactVersion(1, 4, 2);
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
@@ -97,7 +89,6 @@ public class ArtifactVersionTest {
     public void testNotEqualsPatch() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 4, 3);
         final ArtifactVersion a2 = new ArtifactVersion(1, 4, 2);
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
@@ -105,7 +96,6 @@ public class ArtifactVersionTest {
     public void testNotEqualsNull() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 4, 2, "M2");
         final ArtifactVersion a2 = null;
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
@@ -113,56 +103,48 @@ public class ArtifactVersionTest {
     public void testNotEqualsOther() {
         final ArtifactVersion a1 = new ArtifactVersion(1, 4, 2, "M2");
         final Object a2 = new Object();
-        System.out.format("%s not equals %s\n", a1, a2);
         assertNotEquals(a1, a2);
     }
 
     @Test
     public void testOf1() {
         String versionString = "1.4.2";
-        System.out.println("of " + versionString);
         assertEquals(new ArtifactVersion(1, 4, 2), ArtifactVersion.of(versionString));
     }
 
     @Test
     public void testOf2() {
         String versionString = "1.2.3-RC";
-        System.out.println("of " + versionString);
         assertEquals(new ArtifactVersion(1, 2, 3, "RC"), ArtifactVersion.of(versionString));
     }
 
     @Test
     public void testOf3() {
         String versionString = "3.2.1.M2";
-        System.out.println("of " + versionString);
         assertEquals(new ArtifactVersion(3, 2, 1, "M2"), ArtifactVersion.of(versionString));
     }
 
     @Test
     public void testOf4() {
         String versionString = "1.2.3-m4";
-        System.out.println("of " + versionString);
         assertEquals(new ArtifactVersion(1, 2, 3, "M4"), ArtifactVersion.of(versionString));
     }
 
     @Test
     public void testOf9() {
         String versionString = "1.2.3.build-snapshot";
-        System.out.println("of " + versionString);
         assertEquals(new ArtifactVersion(1, 2, 3, "BUILD-SNAPSHOT"), ArtifactVersion.of(versionString));
     }
 
     @Test
     public void testOf5() {
         String versionString = "1.2.3_INVALID";
-        System.out.println("of " + versionString);
         assertThrows(IllegalArgumentException.class, () -> ArtifactVersion.of(versionString));
     }
 
     @Test
     public void testOf6() {
         String versionString = "1.a.3";
-        System.out.println("of " + versionString);
         assertThrows(IllegalArgumentException.class, () -> ArtifactVersion.of(versionString));
     }
 
@@ -188,7 +170,6 @@ public class ArtifactVersionTest {
     public void testCompareTo1() {
         ArtifactVersion one = new ArtifactVersion(1, 1, 2);
         ArtifactVersion two = new ArtifactVersion(1, 1, 2);
-        System.out.format("compare %s = %s\n", one, two);
         assertEquals(0, one.compareTo(two));
     }
 
@@ -196,7 +177,6 @@ public class ArtifactVersionTest {
     public void testCompareTo2() {
         ArtifactVersion one = new ArtifactVersion(1, 1, 2, "M1");
         ArtifactVersion two = new ArtifactVersion(1, 1, 2, "M1");
-        System.out.format("compare %s = %s\n", one, two);
         assertEquals(0, one.compareTo(two));
     }
 
@@ -204,7 +184,6 @@ public class ArtifactVersionTest {
     public void testCompareTo3() {
         ArtifactVersion one = new ArtifactVersion(1, 1, 2);
         ArtifactVersion two = new ArtifactVersion(2, 0, 1);
-        System.out.format("compare %s < %s\n", one, two);
         assertEquals(-1, one.compareTo(two));
     }
 
@@ -212,7 +191,6 @@ public class ArtifactVersionTest {
     public void testCompareTo4() {
         ArtifactVersion one = new ArtifactVersion(1, 0, 13);
         ArtifactVersion two = new ArtifactVersion(1, 1, 1);
-        System.out.format("compare %s < %s\n", one, two);
         assertEquals(-1, one.compareTo(two));
     }
 
@@ -220,7 +198,6 @@ public class ArtifactVersionTest {
     public void testCompareTo5() {
         ArtifactVersion one = new ArtifactVersion(1, 0, 1, "M4");
         ArtifactVersion two = new ArtifactVersion(1, 0, 1, "RC1");
-        System.out.format("compare %s < %s\n", one, two);
         assertTrue(one.compareTo(two) < 0);
     }
 
@@ -228,7 +205,6 @@ public class ArtifactVersionTest {
     public void testCompareTo6() {
         ArtifactVersion one = new ArtifactVersion(1, 0, 1, "RC1");
         ArtifactVersion two = new ArtifactVersion(1, 0, 1, "RC2");
-        System.out.format("compare %s < %s\n", one, two);
         assertTrue(one.compareTo(two) < 0);
     }
 
@@ -236,7 +212,6 @@ public class ArtifactVersionTest {
     public void testCompareTo7() {
         ArtifactVersion one = new ArtifactVersion(1, 0, 1, "RC2");
         ArtifactVersion two = new ArtifactVersion(1, 0, 1, "SNAPSHOT");
-        System.out.format("compare %s < %s\n", one, two);
         assertTrue(one.compareTo(two) < 0);
     }
 
@@ -244,8 +219,14 @@ public class ArtifactVersionTest {
     public void testCompareTo8() {
         ArtifactVersion one = new ArtifactVersion(1, 0, 1, "SNAPSHOT");
         ArtifactVersion two = new ArtifactVersion(1, 0, 1);
-        System.out.format("compare %s < %s\n", one, two);
         assertTrue(one.compareTo(two) < 0);
+    }
+
+    @Test
+    public void testHashCode() {
+        final ArtifactVersion instance = new ArtifactVersion(1, 0, 1, "SNAPSHOT");
+
+        assertEquals(1071061819, instance.hashCode());
     }
 
     @Test
@@ -268,7 +249,6 @@ public class ArtifactVersionTest {
             .sorted()
             .toList();
 
-        System.out.format("sort %s\n", versions);
         final List<ArtifactVersion> progression = List.of(
             new ArtifactVersion(2, 3, 0, "M1"),
             new ArtifactVersion(2, 3, 0, "M2"),
